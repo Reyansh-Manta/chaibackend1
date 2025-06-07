19e856e50c592aaded24a90c08834c3860cd23ba
Reyansh-Manta <mantareyansh@gmail.com>
2025-06-07 22:03:30 +0530

    great!!!

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ import connectDB from "./db/index.js"
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(err)=>{console.log("App connection error ", err)
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Error",err);
+})
