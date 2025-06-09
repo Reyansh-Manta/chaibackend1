2f186489cb8f923279794f11c6d6e438f69d0f34
Reyansh-Manta <mantareyansh@gmail.com>
2025-06-10 02:39:50 +0530

    made uploadOnCloudinary utility

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,7 +65,32 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessTokens = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
 
+userSchema.methods.generateRefreshTokens = function(){
+        return jwt.sign(
+        {
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 
 export const User = mongoose.model("User", userSchema)
