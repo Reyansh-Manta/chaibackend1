b991f75f95585355e2f30c4ed3c86fb3d8583bb4
Reyansh-Manta <mantareyansh@gmail.com>
2025-06-10 20:05:17 +0530

    fixed app.js and added first route and controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    }))
+    app.use(express.json({limit: lim}))
+    app.use(express.urlencoded({extended:true, limit: lim}))
+    app.use(express.static("public"))
+    app.use(cookieParser())
+    
 export {app}
