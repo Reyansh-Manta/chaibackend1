c02610768ff39f5fe505e464cd6e6eb11dbdb521
Reyansh-Manta <mantareyansh@gmail.com>
2025-06-20 08:12:10 +0530

    first pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -357,6 +357,76 @@ const coverImageLocalPath = req.files?.path
 
     })
 
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiError(404, "couldn,t fetch username from req.params")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "channelsSubscriberTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$channelsSubscriberTo"
+                },
+                isSubscribed: {
+                    $cond:{
+                        if: {$in: [req.user?._id , $subscribers.subscriber]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                username: 1,
+                fullName: 1,
+                email: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar:1,
+                coverImage:1
+            }
+        }
+    ])
+
+if(!channel?.length){
+    throw new ApiError(404, "Channel does not exist")
+}
+
+return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
+
+})    
+
 export {
     registerUser,
     loginUser,
@@ -367,7 +437,8 @@ export {
     updateFullName,
     updateEmail,
     updateAvatar,
-    updateCoverImage
+    updateCoverImage,
+    getUserChannelProfile
  }
 
 // user.controller.js end
