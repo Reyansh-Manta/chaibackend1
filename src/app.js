03fd5a185e3b0a09868195883b43c8c6aaeb033b
Reyansh-Manta <mantareyansh@gmail.com>
2025-06-24 06:42:02 +0530

    changes to app.js

--- src/app.js
+++ src/app.js
@@ -19,12 +19,14 @@ app.use(cookieParser())
 //import routes
 import userRouter from "./routes/user.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 //routes declaration
 //cant use app.get because router are in a different file
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscription", subscriptionRouter)
 
 export { app }
 
