adbff6c8e37f0994be22b7fcd594b3544dd73373
Reyansh-Manta <mantareyansh@gmail.com>
2025-07-12 01:26:52 +0530

    fix: remove unnecessary comma in refreshToken field definition

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,7 +44,7 @@ const userSchema = new Schema(
             required: [true, "Password is required"]
         },
         refreshToken: {
-            type: String,
+            type: String
             // required: true
         }
 
